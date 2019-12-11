c7614e434f1eec7ba399e9d7cce20cb01d55c824
kphacko <karan2000patil@gmail.com>
2019-12-11 14:23:19 +0530

    changes backuped

--- push.php
+++ push.php
@@ -5,20 +5,19 @@ if(isset($_POST['submit']) )
 	$type= $_POST['type'];
 	$s=$_POST['sports'];
 	foreach ($_POST['teams'] as $i) {
-		echo $i;	
+			if($type=='boys'){
+				$q="update olympus_points_b set ".$s."=10 where class='".$i."'";
+				mysqli_query($conn,$q);
+				//echo $q;
+				$q1="update olympus_points_b set ".$s."=-10 where ".$s."=0 and class!='".$i."'";
+				mysqli_query($conn,$q1);
+			}
+			if($type=='girls'){
+				$q="update olympus_points_g set ".$s."=10 where class='".$i."'";
+				mysqli_query($conn,$q);
+				$q1="update olympus_points_g set ".$s."=-10 where ".$s."=0 and class!='".$i."'";
+				mysqli_query($conn,$q1);
+			}
 }
 }
-?>
-<?php
-
-// if(isset($_POST['submit']) )
-// {
-//   $varMovie = $_POST['formMovie'];
-//   $varName = $_POST['formName'];
-//   $varGender = $_POST['formGender'];
-//   $errorMessage = "";
-
-//   // - - - snip - - - 
-// }
-
 ?>
