966aee0f925f390681b6d57057876e0af70729fa
krutikpatel28 <krutikpatel347@gmail.com>
2020-01-20 23:02:07 +0530

    major changs

--- events_inter/athletic.php
+++ events_inter/athletic.php
@@ -174,12 +174,7 @@ while($row=mysqli_fetch_array($r))
 
 
 
-				<!-- Copyright -->
-
-
-
-					<div class="copyright">&copy; Untitled. All rights reserved. Design: <a href="#">By Krutik Patel</a>.</div>
-
+				
 			</div>
 
 		<!-- Scripts -->
